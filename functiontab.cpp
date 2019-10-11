1fd33785813a6b9963ab756a523229dfe17ba39b
TereshkivS <sergij.t@gmail.com>
2019-10-11 14:10:52 +0300

    Add ui

---
+++ functiontab.cpp
@@ -0,0 +1,7 @@
+#include "functiontab.h"
+
+FunctionTab::FunctionTab(double a, double b, double delta, double k)
+    :__a(a), __b(b), __delta(delta), __k(k)
+{
+
+}
---
+++ functiontab.h
@@ -0,0 +1,16 @@
+#ifndef FUNCTIONTAB_H
+#define FUNCTIONTAB_H
+
+
+class FunctionTab
+{
+private:
+    double __a;
+    double __b;
+    double __delta;
+    double __k;
+public:
+    FunctionTab(double a, double b, double delta, double k);
+};
+
+#endif // FUNCTIONTAB_H
