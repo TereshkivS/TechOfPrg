f9c45fe3bcb301a3058bfee1f05bbba9488ef6d1
TereshkivS <sergij.t@gmail.com>
2019-10-13 15:36:05 +0300

    Add functional for 1 lab

--- functiontab.cpp
+++ functiontab.cpp
@@ -1,4 +1,5 @@
 #include "functiontab.h"
+#include <math.h>
 
 FunctionTab::FunctionTab(double a, double b, double delta, double k)
     :__a(a), __b(b), __delta(delta), __k(k)
@@ -12,7 +13,10 @@ QVector<QPair<double, double> > FunctionTab::CalculateTab()
     double rez;
     for(double x = __a; x < __b ; x += __delta)
     {
-        rez =
+        rez = x * __k + tan(x);
+        funcResults.append(QPair <double, double>(x, rez));
     }
+
+    return funcResults;
 }
 
--- mainwindow.cpp
+++ mainwindow.cpp
@@ -3,6 +3,8 @@
 
 #include "functiontab.h"
 
+#include <algorithm>
+
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
@@ -23,4 +25,23 @@ void MainWindow::on_calculateButton_clicked()
                                   ,ui->deltaLineEdit->text().toDouble()
                                   ,ui->kLineEdit->text().toDouble());
 
+    auto resTable = functionTab->CalculateTab();
+
+    ui->resultTable->setColumnCount(2);
+    ui->resultTable->setRowCount(resTable.size());
+    QStringList heading;
+    heading <<  "Delta" << "Result";
+    ui->resultTable->setHorizontalHeaderLabels(heading);
+    for (int i = 0 ; i < resTable.size() ; i++) {
+        ui->resultTable->setItem(i, 0
+                                 , (new QTableWidgetItem(QString::number(resTable.at(i).first))));
+        ui->resultTable->setItem(i, 1
+                                 , (new QTableWidgetItem(QString::number(resTable.at(i).second))));
+    }
+    std::vector<double> results;
+    foreach (auto item, resTable) {
+        results.push_back(item.second);
+    }
+    ui->minLineEdit->setText(QString::number(std::min_element(results.begin(), results.end()) - results.begin()));
+    ui->maxLineEdit->setText(QString::number(*std::min_element(results.begin(), results.end())));
 }
